2e3a493efa8b420c63c300b44a4264fa5aa56e42
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-04-17 15:13:29 +0200

    Resolved conflicts.

--- Client/frostbite/FrostbiteRconPacket.h
+++ Client/frostbite/FrostbiteRconPacket.h
@@ -73,20 +73,6 @@ private:
     unsigned int packetWordCount; // Number of words following the packet header
     QVector<FrostbiteRconWord> packetWords; // RconWord packetWords[MAX_WORDS];
 
-<<<<<<< HEAD
-=======
-    QVector<FrostbiteRconWord> packetWords;
-
-    enum Origin {
-        ServerOrigin,
-        ClientOrigin
-    };
-
-    enum {
-        Request,
-        Response
-    };
->>>>>>> master
 };
 
 inline QDataStream &operator >> (QDataStream &in, FrostbiteRconPacket &packet)
