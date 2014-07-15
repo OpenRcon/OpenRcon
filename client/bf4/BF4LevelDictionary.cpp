47a19147f6468b8f37d53401756b250f462da04a
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-15 12:44:36 +0200

    (BF4) Added gamemode Chain Link.

--- client/bf4/BF4LevelDictionary.cpp
+++ client/bf4/BF4LevelDictionary.cpp
@@ -25,9 +25,9 @@
 template<>
 QList<TeamEntry> BF4LevelDictionary::teamList = {
     /* List of all teams */
-    TeamEntry("Neutral", ""),              // -1
-    TeamEntry("RU", ":/bf4/teams/RU.png"), // 0
-    TeamEntry("US", ":/bf4/teams/US.png"), // 1
+    TeamEntry("Neutral", ""),              // 0
+    TeamEntry("RU", ":/bf4/teams/RU.png"), // 1
+    TeamEntry("US", ":/bf4/teams/US.png"), // 2
     TeamEntry("CN", ":/bf4/teams/CN.png"), // 3
 //    TeamEntry("CH", "") // TODO: This is from documentation, but seems to be a typo of CN.
 };
@@ -35,40 +35,40 @@ QList<TeamEntry> BF4LevelDictionary::teamList = {
 template<>
 QList<LevelEntry> BF4LevelDictionary::levelList = {
     /* List of all levels */
-    LevelEntry("MP_Abandoned", "Zavod 311", QList<int>({ 0, 1 }), ":/bf4/levels/zavod_311.png"),                    // 0
-    LevelEntry("MP_Damage", "Lancang Dam", QList<int>({ 0, 2 }), ":/bf4/levels/lancang_dam.png"),                   // 1
-    LevelEntry("MP_Flooded", "Flood Zone", QList<int>({ 1, 2 }), ":/bf4/levels/flood_zone.png"),                    // 2
-    LevelEntry("MP_Journey", "Golmud Railway", QList<int>({ 0, 2 }), ":/bf4/levels/golmud_railway.png"),            // 3
-    LevelEntry("MP_Naval", "Paracel Storm", QList<int>({ 1, 2 }), ":/bf4/levels/paracel_storm.png"),                // 4
-    LevelEntry("MP_Prison", "Operation Locker", QList<int>({ 1, 0 }), ":/bf4/levels/operation_locker.png"),         // 5
-    LevelEntry("MP_Resort", "Hainan Resort", QList<int>({ 1, 2 }), ":/bf4/levels/hainan_resort.png"),               // 6
-    LevelEntry("MP_Siege", "Siege of Shanghai", QList<int>({ 1, 2 }), ":/bf4/levels/seige_of_shanghai.png"),        // 7
-    LevelEntry("MP_TheDish", "Rogue Transmission", QList<int>({ 0, 2 }), ":/bf4/levels/rogue_transmission.png"),    // 8
-    LevelEntry("MP_Tremors", "Dawnbreaker", QList<int>({ 1, 2 }), ":/bf4/levels/dawnbreaker.png"),                  // 9
+    LevelEntry("MP_Abandoned", "Zavod 311", QList<int>({ 1, 2 }), ":/bf4/levels/zavod_311.png"),                    // 0
+    LevelEntry("MP_Damage", "Lancang Dam", QList<int>({ 1, 3 }), ":/bf4/levels/lancang_dam.png"),                   // 1
+    LevelEntry("MP_Flooded", "Flood Zone", QList<int>({ 2, 3 }), ":/bf4/levels/flood_zone.png"),                    // 2
+    LevelEntry("MP_Journey", "Golmud Railway", QList<int>({ 1, 3 }), ":/bf4/levels/golmud_railway.png"),            // 3
+    LevelEntry("MP_Naval", "Paracel Storm", QList<int>({ 2, 3 }), ":/bf4/levels/paracel_storm.png"),                // 4
+    LevelEntry("MP_Prison", "Operation Locker", QList<int>({ 2, 1 }), ":/bf4/levels/operation_locker.png"),         // 5
+    LevelEntry("MP_Resort", "Hainan Resort", QList<int>({ 2, 3 }), ":/bf4/levels/hainan_resort.png"),               // 6
+    LevelEntry("MP_Siege", "Siege of Shanghai", QList<int>({ 2, 3 }), ":/bf4/levels/seige_of_shanghai.png"),        // 7
+    LevelEntry("MP_TheDish", "Rogue Transmission", QList<int>({ 1, 3 }), ":/bf4/levels/rogue_transmission.png"),    // 8
+    LevelEntry("MP_Tremors", "Dawnbreaker", QList<int>({ 2, 3 }), ":/bf4/levels/dawnbreaker.png"),                  // 9
 
     // XP1 (China Rising)
-    LevelEntry("XP1_001", "Silk Road", QList<int>({ 1, 2 }), ":/bf4/levels/silk_road.png"),                         // 10
-    LevelEntry("XP1_002", "Altai Range", QList<int>({ 1, 2 }), ":/bf4/levels/altai_range.png"),                     // 11
-    LevelEntry("XP1_003", "Guilin Peaks", QList<int>({ 1, 2 }), ":/bf4/levels/guilin_peaks.png"),                   // 12
-    LevelEntry("XP1_004", "Dragon Pass", QList<int>({ 1, 2 }), ":/bf4/levels/dragon_pass.png"),                     // 13
+    LevelEntry("XP1_001", "Silk Road", QList<int>({ 2, 3 }), ":/bf4/levels/silk_road.png"),                         // 10
+    LevelEntry("XP1_002", "Altai Range", QList<int>({ 2, 3 }), ":/bf4/levels/altai_range.png"),                     // 11
+    LevelEntry("XP1_003", "Guilin Peaks", QList<int>({ 2, 3 }), ":/bf4/levels/guilin_peaks.png"),                   // 12
+    LevelEntry("XP1_004", "Dragon Pass", QList<int>({ 2, 3 }), ":/bf4/levels/dragon_pass.png"),                     // 13
 
     // XP2 (Second Assault)
-    LevelEntry("XP0_Caspian", "Caspian Border 2014", QList<int>({ 1, 0 }), ":/bf4/levels/caspian_border_2014.png"), // 14
-    LevelEntry("XP0_Firestorm", "Firestorm 2014", QList<int>({ 1, 0 }), ":/bf4/levels/firestorm_2014.png"),         // 15
-    LevelEntry("XP0_Metro", "Operation Metro 2014", QList<int>({ 1, 0 }), ":/bf4/levels/operation_metro_2014.png"), // 16
-    LevelEntry("XP0_Oman", "Gulf of Oman 2014", QList<int>({ 1, 0 }), ":/bf4/levels/gulf_of_oman_2014.png"),        // 17
+    LevelEntry("XP0_Caspian", "Caspian Border 2014", QList<int>({ 2, 1 }), ":/bf4/levels/caspian_border_2014.png"), // 14
+    LevelEntry("XP0_Firestorm", "Firestorm 2014", QList<int>({ 2, 1 }), ":/bf4/levels/firestorm_2014.png"),         // 15
+    LevelEntry("XP0_Metro", "Operation Metro 2014", QList<int>({ 2, 1 }), ":/bf4/levels/operation_metro_2014.png"), // 16
+    LevelEntry("XP0_Oman", "Gulf of Oman 2014", QList<int>({ 2, 1 }), ":/bf4/levels/gulf_of_oman_2014.png"),        // 17
 
     // XP3 (Naval Strike)
-    LevelEntry("XP2_001", "Lost Islands", QList<int>({ 1, 3 }), ":/bf4/levels/lost_islands.png"),                   // 18
-    LevelEntry("XP2_002", "Nansha Strike", QList<int>({ 1, 3 }), ":/bf4/levels/nansha_strike.png"),                 // 19
-    LevelEntry("XP2_003", "Wave Breaker", QList<int>({ 1, 3 }), ":/bf4/levels/wave_breaker.png"),                   // 20
-    LevelEntry("XP2_004", "Operation Mortar", QList<int>({ 1, 3 }), ":/bf4/levels/operation_mortar.png"),           // 21
+    LevelEntry("XP2_001", "Lost Islands", QList<int>({ 2, 1 }), ":/bf4/levels/lost_islands.png"),                   // 18
+    LevelEntry("XP2_002", "Nansha Strike", QList<int>({ 2, 1 }), ":/bf4/levels/nansha_strike.png"),                 // 19
+    LevelEntry("XP2_003", "Wave Breaker", QList<int>({ 2, 1 }), ":/bf4/levels/wave_breaker.png"),                   // 20
+    LevelEntry("XP2_004", "Operation Mortar", QList<int>({ 2, 1 }), ":/bf4/levels/operation_mortar.png"),           // 21
 
     // XP4 (Dragon's Teeth)
-    LevelEntry("XP3_MarketPl", "Pearl Market", QList<int>({ 0, 0 }), ":/bf4/levels/pearl_market.png"),              // 22
-    LevelEntry("XP3_Prpganda", "Propaganda", QList<int>({ 0, 0 }), ":/bf4/levels/propaganda.png"),                  // 23
-    LevelEntry("XP3_UrbanGdn", "Lumphini Garden", QList<int>({ 0, 0 }), ":/bf4/levels/lumphini_garden.png"),        // 24
-    LevelEntry("XP3_WtrFront", "Sunken Dragon", QList<int>({ 0, 0 }), ":/bf4/levels/sunken_dragon.png"),            // 25
+    LevelEntry("XP3_MarketPl", "Pearl Market", QList<int>({ 2, 3 }), ":/bf4/levels/pearl_market.png"),              // 22
+    LevelEntry("XP3_Prpganda", "Propaganda", QList<int>({ 2, 3 }), ":/bf4/levels/propaganda.png"),                  // 23
+    LevelEntry("XP3_UrbanGdn", "Lumphini Garden", QList<int>({ 2, 3 }), ":/bf4/levels/lumphini_garden.png"),        // 24
+    LevelEntry("XP3_WtrFront", "Sunken Dragon", QList<int>({ 2, 3 }), ":/bf4/levels/sunken_dragon.png"),            // 25
 
     // XP5 (Final Stand)
 };
@@ -76,18 +76,19 @@ QList<LevelEntry> BF4LevelDictionary::levelList = {
 template<>
 QList<BF4GameModeEntry> BF4LevelDictionary::gameModeList = {
     /* List of all gamemodes */
-    BF4GameModeEntry("ConquestLarge0", "Conquest Large", 64, 2, 4),               // 0
-    BF4GameModeEntry("ConquestSmall0", "Conquest Small", 32, 2, 4),               // 1
-    BF4GameModeEntry("Domination0", "Domination", 20, 0, 4),                      // 2
-    BF4GameModeEntry("Elimination0", "Defuse", 10, 0, 4),                         // 3
-    BF4GameModeEntry("Obliteration", "Obliteration", 32, 2, 4),                   // 4
-    BF4GameModeEntry("RushLarge0", "Rush", 32, 2, 4),                             // 5
-    BF4GameModeEntry("SquadDeathMatch0", "Squad Deathmatch", 20, 0, 4),           // 6
-    BF4GameModeEntry("TeamDeathMatch0", "Team Deathmatch", 20, 0, 4),             // 7
-    BF4GameModeEntry("AirSuperiority0", "Air Superiority", 24, 0, 4),             // 8
-    BF4GameModeEntry("CaptureTheFlag0", "Capture the flag", 32, 0, 4),            // 9
-    BF4GameModeEntry("CarrierAssaultLarge0", "Carrier Assault Large", 64, 0, 4),  // 10
-    BF4GameModeEntry("CarrierAssaultSmall0", "Carrier Assault Small", 64, 0, 4)   // 11 Check this when docs are available.
+    BF4GameModeEntry("ConquestLarge0", "Conquest Large", 64, 2, 4),              // 0
+    BF4GameModeEntry("ConquestSmall0", "Conquest Small", 32, 2, 4),              // 1
+    BF4GameModeEntry("Domination0", "Domination", 20, 0, 4),                     // 2
+    BF4GameModeEntry("Elimination0", "Defuse", 10, 0, 4),                        // 3
+    BF4GameModeEntry("Obliteration", "Obliteration", 32, 2, 4),                  // 4
+    BF4GameModeEntry("RushLarge0", "Rush", 32, 2, 4),                            // 5
+    BF4GameModeEntry("SquadDeathMatch0", "Squad Deathmatch", 20, 0, 4),          // 6
+    BF4GameModeEntry("TeamDeathMatch0", "Team Deathmatch", 20, 0, 4),            // 7
+    BF4GameModeEntry("AirSuperiority0", "Air Superiority", 24, 0, 4),            // 8
+    BF4GameModeEntry("CaptureTheFlag0", "Capture the flag", 32, 0, 4),           // 9
+    BF4GameModeEntry("CarrierAssaultLarge0", "Carrier Assault Large", 64, 0, 4), // 10
+    BF4GameModeEntry("CarrierAssaultSmall0", "Carrier Assault Small", 64, 0, 4), // 11 Check this when docs are available.
+    BF4GameModeEntry("Chainlink0", "Chain Link", 64, 0, 4)                         // 11 Check this when docs are available.
 };
 
 template<>
@@ -419,6 +420,12 @@ QMultiHash<int, int> BF4LevelDictionary::levelMap = {
     { 9, 20 },
     { 9, 21 },
 
+    // XP4 (Dragon's Teeth)
+    { 9, 22 },
+    { 9, 23 },
+    { 9, 24 },
+    { 9, 25 },
+
     /* Carrier Assault Large */
 
     // XP3 (Naval Strike)
@@ -433,5 +440,13 @@ QMultiHash<int, int> BF4LevelDictionary::levelMap = {
     { 11, 18 },
     { 11, 19 },
     { 11, 20 },
-    { 11, 21 }
+    { 11, 21 },
+
+    /* Chainlink */
+
+    // XP4 (Dragon's Teeth)
+    { 12, 22 },
+    { 12, 23 },
+    { 12, 24 },
+    { 12, 25 }
 };
